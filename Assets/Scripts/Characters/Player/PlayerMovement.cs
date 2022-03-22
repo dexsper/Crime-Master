f0984ab661cf44732468a203d14f32156d9ba8d3
anonymuspro <dexsper@mail.ru>
2022-03-23 02:30:31 +0600

    Player movement and import cinemachine

--- Assets/Scripts/Characters/Player/PlayerMovement.cs
+++ Assets/Scripts/Characters/Player/PlayerMovement.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using Zenject;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [Header("Base Settings")]
+    [Range(1, 10)]
+    [SerializeField] private float _forwardSpeed = 5f;
+    [Range(1, 3)]
+    [SerializeField] private float _sideSpeed = 1.2f;
+
+    private Rigidbody _rigidbody;
+
+    [Inject]
+    private IInput _input;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector3 direction = new Vector3(_input.Horizontal * _sideSpeed, 0f, _forwardSpeed);
+
+        transform.Translate(direction * Time.deltaTime);
+    }
 }
