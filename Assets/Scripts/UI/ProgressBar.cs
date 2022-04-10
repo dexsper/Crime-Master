3876a43107d104aa62e6ae968e6c5f2d6743ae1c
anonymuspro <dexsper@mail.ru>
2022-04-10 11:04:20 +0600

    Reset progress bar after disable

--- Assets/Scripts/UI/ProgressBar.cs
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -6,6 +6,11 @@ public class ProgressBar : MonoBehaviour
 {
     [SerializeField] private Image _fillImage;
 
+    private void OnDisable()
+    {
+        _fillImage.fillAmount = 0;
+    }
+
     public void SetProgress(float value)
     {
         value = Mathf.Clamp01(value);
