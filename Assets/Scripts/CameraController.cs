933ee07a58b8724e037035fce6db4145dd2b2ed3
anonymuspro <dexsper@mail.ru>
2022-04-10 11:10:02 +0600

    Hide stats ui in city

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using DG.Tweening;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using Zenject;
 
 public class CameraController : MonoBehaviour
@@ -11,6 +12,10 @@ public class CameraController : MonoBehaviour
     [Header("Cameras")]
     [SerializeField] private CinemachineVirtualCamera _playerCamera;
     [SerializeField] private CinemachineVirtualCamera _cityCamera;
+
+    [Header("Events")]
+    public UnityEvent OnCityShow;
+    public UnityEvent OnPlayerShow;
 
     [Inject]
     private IInput _input;
@@ -22,6 +27,8 @@ public class CameraController : MonoBehaviour
     {
         _city.gameObject.SetActive(true);
         _playerCamera.Priority = -1;
+
+        OnCityShow?.Invoke();
         DOTween.To(() => _cityCamera.m_Lens.FieldOfView, x => _cityCamera.m_Lens.FieldOfView = x, 70, 1.5f).SetEase(Ease.InOutBack);
     }
     public void ShowPlayer()
@@ -31,6 +38,7 @@ public class CameraController : MonoBehaviour
             _playerCamera.Priority = 1;
             _input.Enabled = true;
             _player.Movement.enabled = true;
+            OnPlayerShow?.Invoke();
         });
     }
 }
